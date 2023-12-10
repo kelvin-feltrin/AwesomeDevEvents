8f8aa04feb91a990071f712c7c9b875739308996
kelvin-feltrin <kelvin-798@hotmail.com>
2023-12-09 21:13:47 -0300

    Criacao do CRUD com ASP NET Core do projeto de REST API

---
+++ AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeDevEvents.API.Entities;
+
+namespace AwesomeDevEvents.API.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+
+        public List<DevEvent> DevEvents { get; set; }
+    }
+}
