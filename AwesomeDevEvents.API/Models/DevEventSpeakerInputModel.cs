581c03a7c766b37d05b143cef182a6323cb8b4ec
kelvin-feltrin <kelvin-798@hotmail.com>
2023-12-17 20:35:52 -0300

    Mapeamento de objetos com AutoMapper

---
+++ AwesomeDevEvents.API/Models/DevEventSpeakerInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AwesomeDevEvents.API.Models
+{
+    public class DevEventSpeakerInputModel
+    {
+        public string Name { get; set; }
+
+        public string TalkTitle { get; set; }
+
+        public string TalkDescription { get; set; }
+
+        public string LinkedinProfile { get; set; }
+    }
+}
